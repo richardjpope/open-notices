1cab0697408f1189ada1bf14df55b95487a6cae3
Richard Pope <richardpope@memespring.local>
2016-10-30 21:55:44 +0000

    config vars for production

--- community/settings/production.py
+++ community/settings/production.py
@@ -13,7 +13,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_FROM_ADDRESS = 'mail@localhost'
 
 # Database
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # BROKER_URL = 'redis://localhost:6379/0'
