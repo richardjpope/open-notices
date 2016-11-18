615c660332c344f67bebc8a90c981dee968d119e
Richard Pope <richardpope@memespring.local>
2016-11-18 12:16:44 +0000

    menu

--- community/apps/notices/urls.py
+++ community/apps/notices/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^/?$', views.NoticeList.as_view(), name='notice-list'),
+    url(r'^$', views.NoticeList.as_view(), name='notice-list'),
     url(r'^/new$', views.NoticeCreate.as_view(), name='notice-create'),
     url(r'^/(?P<pk>[0-9]+)/?$', views.NoticeDetail.as_view(), name='notice-detail'),
 ]
