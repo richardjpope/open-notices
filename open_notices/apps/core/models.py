29fdd5588a421cc29b49745d190e289947cccb7c
Richard Pope <richardpope@memespring.local>
2016-12-07 16:45:14 +0000

    tests passing

--- open_notices/apps/core/models.py
+++ open_notices/apps/core/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import pre_save
 class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
 
-    display_name = models.CharField(unique=True, blank=True, max_length=100, help_text="This name will be displayed publicly if you post any notices")
+    display_name = models.CharField(unique=True, blank=True, null=True,max_length=100, help_text="This name will be displayed publicly if you post any notices")
     email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_superuser  = models.BooleanField(default=False)
