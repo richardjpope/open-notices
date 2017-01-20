df637968d1f6d4cb86ebbdd1f7a0c8bbe5609964
Richard Pope <richardpope@memespring.local>
2017-01-20 11:39:12 +0000

    password hashing properly

--- open_notices/apps/core/forms.py
+++ open_notices/apps/core/forms.py
@@ -8,3 +8,10 @@ class RegistrationForm(forms.ModelForm):
         fields = ['email', 'password', 'display_name']
 
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
