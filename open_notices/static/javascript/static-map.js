5ab9167487c7d7ab89a623d01b1af2a994cfce54
Richard Pope <richardpope@memespring.local>
2017-01-21 11:24:08 +0000

    fixed issues with multiple alert maps not displaying / use toner map on create alert closes #70

--- open_notices/static/javascript/static-map.js
+++ open_notices/static/javascript/static-map.js
@@ -1,13 +1,13 @@
   function staticMap(map_id, geojson_url){
 
     //map
-    geoJSONFormat = new ol.format.GeoJSON();
-    source = new ol.source.Vector({
+    var geoJSONFormat = new ol.format.GeoJSON();
+    var source = new ol.source.Vector({
           format: new ol.format.GeoJSON(),
           url: geojson_url
       });
-    raster = new ol.layer.Tile({source: new ol.source.Stamen({layer: 'toner'})});
-    vector = new ol.layer.Vector({
+    var raster = new ol.layer.Tile({source: new ol.source.Stamen({layer: 'toner'})});
+    var vector = new ol.layer.Vector({
       source: source,
       style: new ol.style.Style({
           stroke: new ol.style.Stroke({
@@ -22,7 +22,7 @@
                       })
       })
     });
-    map = new ol.Map({
+    var map = new ol.Map({
         layers: [raster, vector],
         target: map_id,
         controls: ol.control.defaults({zoom:false, rotate:false}),
@@ -35,7 +35,6 @@
         buffer_size = 250; //no idea what the units are here!
         buffered_extent = ol.extent.buffer(source.getExtent(), buffer_size);
         map.getView().fit(buffered_extent, map.getSize());
-        // map.getView().setZoom(16)
       }
     });
   }
